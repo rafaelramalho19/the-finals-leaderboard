d89aeffbb08282450fc72f293bf8dfc367c14c85
Rafael Ramalho <rafael.ramalho@olx.com>
2023-03-08 19:28:25 +0100

    feat: add leaderboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,72 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+type User = {
+  name: string;
+  r: number;
+  f: number;
+  of: number;
+  or: number;
+  x: number;
+  mx: number;
+  c: number;
+};
+
+const fetchLeaderboard = async () => {
+  try {
+    return (
+      await fetch(
+        "https://embark-discovery-leaderboard.storage.googleapis.com/leaderboard.json"
+      )
+    ).json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const FameIcon = () => (
+  <svg
+    className={styles.icon}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="#ffd700"
+    height="16"
+    width="16"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M10 1c-1.828 0-3.623.149-5.371.435a.75.75 0 00-.629.74v.387c-.827.157-1.642.345-2.445.564a.75.75 0 00-.552.698 5 5 0 004.503 5.152 6 6 0 002.946 1.822A6.451 6.451 0 017.768 13H7.5A1.5 1.5 0 006 14.5V17h-.75C4.56 17 4 17.56 4 18.25c0 .414.336.75.75.75h10.5a.75.75 0 00.75-.75c0-.69-.56-1.25-1.25-1.25H14v-2.5a1.5 1.5 0 00-1.5-1.5h-.268a6.453 6.453 0 01-.684-2.202 6 6 0 002.946-1.822 5 5 0 004.503-5.152.75.75 0 00-.552-.698A31.804 31.804 0 0016 2.562v-.387a.75.75 0 00-.629-.74A33.227 33.227 0 0010 1zM2.525 4.422C3.012 4.3 3.504 4.19 4 4.09V5c0 .74.134 1.448.38 2.103a3.503 3.503 0 01-1.855-2.68zm14.95 0a3.503 3.503 0 01-1.854 2.68C15.866 6.449 16 5.74 16 5v-.91c.496.099.988.21 1.475.332z"
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+);
 
 export default function Home() {
+  const [leaderboard, setLeaderboard] = useState<User[]>([]);
+  const [user, setUser] = useState<User | null>(null);
+
+  const getLeaderboard = async () => {
+    const leaderboard = await fetchLeaderboard();
+
+    const raising = leaderboard.find(
+      (user: User) => user.name === "TTV/Raisingz"
+    );
+
+    setUser(raising);
+    setLeaderboard(leaderboard.splice(0, 3));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getLeaderboard();
+    }, 10000);
+
+    getLeaderboard();
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Head>
@@ -15,109 +76,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        {leaderboard?.map((user: User) => (
+          <div key={user.name} className={styles.user}>
+            <div>[{user.r}]</div>
+            <div>{user.name}</div>
+            <div>
+              <FameIcon /> {user.f}
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+        ))}
+        <hr className={styles.divider} />
+        {user ? (
+          <div key={user.name} className={styles.raising}>
+            <div className={styles.user}>
+              <div>[{user.r}]</div>
+              <div>{user.name}</div>
+              <div>
+                {" "}
+                <FameIcon /> {user.f}
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        ) : null}
       </main>
     </>
-  )
+  );
 }
